c552d402b31fbe68a7b9db1605c433070dcecb36
joona0306 <dev.joona0306@gmail.com>
2025-06-04 23:28:02 +0900

    feat: sw-card 스와이퍼 breakpoints 기능 연구

--- js/swCard.js
+++ js/swCard.js
@@ -1,8 +1,30 @@
 const swCard = new Swiper(".sw-card", {
- slidesPerView: 3,
- spaceBetween: 30,
- pagination: {
-  el: ".swiper-pagination",
-  clickable: true,
- },
+  centeredSlides: false,
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      centeredSlides: true,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      centeredSlides: false,
+    },
+    1220: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      centeredSlides: false,
+    },
+  },
 });
