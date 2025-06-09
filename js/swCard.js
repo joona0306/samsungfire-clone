85974f8fd7bdd5555e33f7e300db83fc9f0495b6
joona0306 <dev.joona0306@gmail.com>
2025-06-09 13:04:47 +0900

    design: desktop 화면 퍼블리싱 완료

--- js/swCard.js
+++ js/swCard.js
@@ -1,32 +1,32 @@
 const swCard = new Swiper(".sw-card", {
-  // 기본값
-  centeredSlides: false,
-  slidesPerView: "auto",
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+ // 기본값
+ centeredSlides: false,
+ slidesPerView: "auto",
+ spaceBetween: 30,
+ pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+ },
+ navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+ },
+ //  반응형
+ breakpoints: {
+  // window.width >= 0
+  0: {
+   centeredSlides: true,
+   spaceBetween: 20,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  // window.width >= 768
+  768: {
+   centeredSlides: false,
+   spaceBetween: 30,
   },
-  //  반응형
-  breakpoints: {
-    // window.width >= 0
-    0: {
-      centeredSlides: true,
-      spaceBetween: "1.3rem",
-    },
-    // window.width >= 768
-    768: {
-      centeredSlides: false,
-      spaceBetween: 30,
-    },
-    // window.width >= 1220
-    1220: {
-      centeredSlides: false,
-      spaceBetween: 30,
-    },
+  // window.width >= 1220
+  1220: {
+   centeredSlides: false,
+   spaceBetween: 30,
   },
+ },
 });
